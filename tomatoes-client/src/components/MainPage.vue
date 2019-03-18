e19f794364e8fbb652fe32a0121d80c823eac425
annndrey <annndrey@trololo.info>
2019-03-18 14:11:38 +0300

    removed crop preview

--- tomatoes-client/src/components/MainPage.vue
+++ tomatoes-client/src/components/MainPage.vue
@@ -29,7 +29,7 @@
 	    <div class="col-md-12" v-if="row.imageURL">
 	      <div class="card">
 		<clipper-fixed :src="row.imageURL" :preview="'fixed-preview'+index" :ref="'clipper'+index"></clipper-fixed>
-		<clipper-preview :name="'fixed-preview'+index"></clipper-preview>
+		<!--<clipper-preview :name="'fixed-preview'+index"></clipper-preview>-->
 		<div class="card-body">
 		  <h5 class="card-title">{{row.fileMessage}}</h5>
 		  <p v-if="row.plantType" class="card-text">Plant Type: {{row.plantType}}<br>Plant Status: {{row.plantStatus}}</p>
