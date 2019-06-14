d824bbc7f0e95f3a1e8d484131832a6234660231
annndrey <gontchar@gmail.com>
2019-06-14 15:45:02 +0300

    Added a modified version for agrodozor & phytoftora demo

--- tomatoes-client/src/components/MainPage.vue
+++ tomatoes-client/src/components/MainPage.vue
@@ -4,15 +4,19 @@
     <form v-on:submit.prevent="uploadImages">
       <div class="row mt-4 " v-for="(row, index) in formRows">
 	<div class="col">
-	
-	  <div class="row mt-4 ">
+	  <div class="row mt-4">
+	    <div class="col-12">
+	      <span class="float-right ml-3"><small>Upoad photo and select a leaf on it</small></span>
+	    </div>
+	  </div>
+	  <div class="row ">
 	    <div class="col-5 ">
 		<button class="btn btn-outline-secondary float-left" type="button" id="inputAddNewInput" @click="addFileUploadField"><img height="11px" src="@/assets/plus.svg"></button>
 		<button class="btn btn-outline-secondary float-left" type="button" id="inputDeleteInput" @click="clearFields(index)"><img height="13px" src="@/assets/trash.svg"></button>
 	    </div>
 	    <div class="col-7 ">
 	      <clipper-upload v-model="row.imageURL" >
-		<button class="btn btn-outline-secondary" style="width:100%" type="button"><img height="16px" src="@/assets/aperture.svg"></button>
+		<button class="btn btn-outline-secondary" style="width:100%" type="button"><img height="16px" src="@/assets/aperture.svg"> </button>
 	      </clipper-upload>
 	    </div>
 
